6df143ad91603af369d35365781c3446d297a0c2
qikang <786960910@qq.com>
2020-05-14 15:39:57 +0800

    查看课件

---
+++ modules/document.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python
+# -*- encoding: utf-8 -*-
+"""
+@File    : document.py
+@Time    : 2020/5/14 14:25
+@Author  : Qi
+@Email   : 18821723039@163.com
+@Software: PyCharm
+"""
+from flask import g
+
+from main import mongo
+from utils import response_code
+from utils.setResJson import set_resjson
+
+
+class DocumentHandler(object):
+    def __init__(self, extra_data, model_action):
+        self.extra_data = extra_data
+        self.model_action = model_action
+
+    def handle_model(self):
+        func_name = 'func_{}'.format(self.model_action)
+        func_name = func_name.lower()
+        try:
+            handle_function = getattr(DocumentHandler, func_name)
+            resp = handle_function(self)
+        except AttributeError as e:
+            resp = set_resjson(err=-4,
+                               errmsg="{} is incorrect !".format(
+                                   self.model_action))
+        return resp
+
+    def func_view_file(self):
+        """
+        查看课件
+        """
+        user = g.user
+        if not user:
+            raise response_code.UserERR(errmsg='用户未登录')
+        video_id = self.extra_data.get('video_id')
+        try:
+            video_document = mongo.db.document.find_one({"video_id": video_id},
+                                                        {"_id": 0,
+                                                         "file_name": 1,
+                                                         "file_path": 1,
+                                                         "image_path": 1,
+                                                         "price": 1,
+                                                         "time": 1,
+                                                         })
+        except Exception as e:
+            raise response_code.DatabaseERR(errmsg="{}".format(e))
+        if not video_document:
+            raise response_code.RoleERR(errmsg="这个视频没有课件")
+
+        return set_resjson(res_array=[video_document])
